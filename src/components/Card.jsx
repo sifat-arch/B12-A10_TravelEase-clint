63cdafe77af314d23a888d5432b99d74956257b4
sifat-arch <sifatullah1004@gmail.com>
2025-11-10 00:36:13 +0600

    added all vehicles component

---
+++ src/components/Card.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Card = ({ vehicles }) => {
+  return (
+    <div className=" rounded-2xl overflow-hidden shadow-lg bg-white">
+      <img
+        className="w-full h-48 object-cover"
+        src={vehicles.coverImage}
+        alt={vehicles.vehicleName}
+      />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{vehicles.vehicleName}</div>
+        <p className="text-gray-700 text-base mb-1">Owner: {vehicles.owner}</p>
+
+        <p className="text-gray-700 text-base mb-1">
+          Price per day: ${vehicles.pricePerDay}
+        </p>
+
+        <span
+          className={`inline-block px-3 py-1 text-sm font-semibold rounded-full text-center`}
+        >
+          {vehicles.availability}
+        </span>
+      </div>
+      <div className="px-6 py-4 flex justify-between">
+        <button className="w-full bg-gray-500 text-white px-4 py-2 rounded-lg hover:bg-gray-600">
+          Details
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/pages/AllVehicles.jsx
+++ src/pages/AllVehicles.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useAxios from "../hooks/useAxios";
+import Card from "../components/Card";
 
 const AllVehicles = () => {
-  return <div>AllVehicles</div>;
+  const [allVehicles, setAllVehicles] = useState([]);
+  const asioxInstnce = useAxios();
+  useEffect(() => {
+    asioxInstnce.get("/vehicles").then((data) => {
+      setAllVehicles(data.data);
+    });
+  }, [asioxInstnce]);
+  return (
+    <div>
+      <h1>All Vehicles</h1>
+      <div className="grid grid-cols-4 gap-6">
+        {allVehicles.map((vehicles) => (
+          <Card key={vehicles.id} vehicles={vehicles} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AllVehicles;
