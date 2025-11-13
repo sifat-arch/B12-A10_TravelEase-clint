9a6b43268077be6ba9e069123d9c1bcbc6b831d7
sifat-arch <sifatullah1004@gmail.com>
2025-11-13 13:16:34 +0600

    Initial commit with Firebase setup

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,19 +1,38 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAqAScjpDcsxZZ3B8zFrLcMsMyDjQPMPbU",
+//   authDomain: "travelease-b4b15.firebaseapp.com",
+//   projectId: "travelease-b4b15",
+//   storageBucket: "travelease-b4b15.firebasestorage.app",
+//   messagingSenderId: "142712300841",
+//   appId: "1:142712300841:web:1e6faaeb8dbba088654338",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration (from .env)
 const firebaseConfig = {
-  apiKey: "AIzaSyAqAScjpDcsxZZ3B8zFrLcMsMyDjQPMPbU",
-  authDomain: "travelease-b4b15.firebaseapp.com",
-  projectId: "travelease-b4b15",
-  storageBucket: "travelease-b4b15.firebasestorage.app",
-  messagingSenderId: "142712300841",
-  appId: "1:142712300841:web:1e6faaeb8dbba088654338",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export default app;
